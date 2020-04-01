95442b0da0457f1344bad790182711b7ac93f87d
Guilherme Severo <guilhermehmsevero@hotmail.com>
2020-03-31 23:07:18 -0300

    Implementado novos testes para conversão de Decimal to romano

--- tests/test_decimal_to_roman.py
+++ tests/test_decimal_to_roman.py
@@ -26,6 +26,47 @@ class TestDecimalToRoman(unittest.TestCase):
     def test_convert_to_M(self):
         self.assertEqual(to_roman(1000), 'M', 'Convert To M')
 
+    def test_convert_to_III(self):
+        self.assertEqual(to_roman(3), 'III', 'Convert To III')
+
+    def test_convert_to_IX(self):
+        self.assertEqual(to_roman(9), 'IX', 'Convert To IX')
+
+    def test_convert_to_XIV(self):
+        self.assertEqual(to_roman(14), 'XIV', 'Convert To XIV')
+
+    def test_convert_to_(self):
+        self.assertEqual(to_roman(38), 'XXXVIII', 'Convert To XXXVIII')
+
+    def test_convert_to_LXXVII(self):
+        self.assertEqual(to_roman(77), 'LXXVII', 'Convert To LXXVII')
+
+    def test_convert_to_XCII(self):
+        self.assertEqual(to_roman(92), 'XCII', 'Convert To XCII')
+
+    def test_convert_to_CCXXXVIII(self):
+        self.assertEqual(to_roman(238), 'CCXXXVIII', 'Convert To CCXXXVIII')
+
+    def test_convert_to_DCCXIV(self):
+        self.assertEqual(to_roman(714), 'DCCXIV', 'Convert To DCCXIV')
+
+    def test_convert_to_MCXCVIII(self):
+        self.assertEqual(to_roman(1198), 'MCXCVIII', 'Convert To MCXCVIII')
+
+    def test_convert_to_MCCXL(self):
+        self.assertEqual(to_roman(1240), 'MCCXL', 'Convert To MCCXL')
+
+    def test_convert_to_DIV(self):
+        self.assertEqual(to_roman(504), 'DIV', 'Convert To DIV')
+
+    def test_type_error_int(self):
+        with self.assertRaises(TypeError):
+            to_roman('1')
+
+    def test_type_error_list(self):
+        with self.assertRaises(TypeError):
+            to_roman([1, 92])
+
 
 if __name__ == '__main__':
     unittest.main()

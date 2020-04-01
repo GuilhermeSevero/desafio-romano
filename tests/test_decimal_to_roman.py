5ad4a2be1ae0ab4e8ee789d3d578c166fa25d214
Guilherme Severo <guilhermehmsevero@hotmail.com>
2020-04-01 20:23:03 -0300

    Adicionado dinamismo aos testes.

--- tests/test_decimal_to_roman.py
+++ tests/test_decimal_to_roman.py
@@ -5,59 +5,35 @@ from roman import to_roman
 
 class TestDecimalToRoman(unittest.TestCase):
 
-    def test_convert_to_I(self):
-        self.assertEqual(to_roman(1), 'I', 'Convert To I')
+    def setUp(self) -> None:
+        self.test_cases = (
+            (1, 'I'),
+            (3, 'III'),
+            (4, 'IV'),
+            (5, 'V'),
+            (8, 'VIII'),
+            (9, 'IX'),
+            (10, 'X'),
+            (38, 'XXXVIII'),
+            (40, 'XL'),
+            (50, 'L'),
+            (77, 'LXXVII'),
+            (100, 'C'),
+            (138, 'CXXXVIII'),
+            (170, 'CLXX'),
+            (215, 'CCXV'),
+            (238, 'CCXXXVIII'),
+            (390, 'CCCXC'),
+            (483, 'CDLXXXIII'),
+            (500, 'D'),
+            (1000, 'M'),
+            (1198, 'MCXCVIII'),
+            (1240, 'MCCXL'),
+        )
 
-    def test_convert_to_V(self):
-        self.assertEqual(to_roman(5), 'V', 'Convert To V')
-
-    def test_convert_to_X(self):
-        self.assertEqual(to_roman(10), 'X', 'Convert To X')
-
-    def test_convert_to_L(self):
-        self.assertEqual(to_roman(50), 'L', 'Convert To L')
-
-    def test_convert_to_C(self):
-        self.assertEqual(to_roman(100), 'C', 'Convert To C')
-
-    def test_convert_to_D(self):
-        self.assertEqual(to_roman(500), 'D', 'Convert To D')
-
-    def test_convert_to_M(self):
-        self.assertEqual(to_roman(1000), 'M', 'Convert To M')
-
-    def test_convert_to_III(self):
-        self.assertEqual(to_roman(3), 'III', 'Convert To III')
-
-    def test_convert_to_IX(self):
-        self.assertEqual(to_roman(9), 'IX', 'Convert To IX')
-
-    def test_convert_to_XIV(self):
-        self.assertEqual(to_roman(14), 'XIV', 'Convert To XIV')
-
-    def test_convert_to_(self):
-        self.assertEqual(to_roman(38), 'XXXVIII', 'Convert To XXXVIII')
-
-    def test_convert_to_LXXVII(self):
-        self.assertEqual(to_roman(77), 'LXXVII', 'Convert To LXXVII')
-
-    def test_convert_to_XCII(self):
-        self.assertEqual(to_roman(92), 'XCII', 'Convert To XCII')
-
-    def test_convert_to_CCXXXVIII(self):
-        self.assertEqual(to_roman(238), 'CCXXXVIII', 'Convert To CCXXXVIII')
-
-    def test_convert_to_DCCXIV(self):
-        self.assertEqual(to_roman(714), 'DCCXIV', 'Convert To DCCXIV')
-
-    def test_convert_to_MCXCVIII(self):
-        self.assertEqual(to_roman(1198), 'MCXCVIII', 'Convert To MCXCVIII')
-
-    def test_convert_to_MCCXL(self):
-        self.assertEqual(to_roman(1240), 'MCCXL', 'Convert To MCCXL')
-
-    def test_convert_to_DIV(self):
-        self.assertEqual(to_roman(504), 'DIV', 'Convert To DIV')
+    def test_decimal_to_roman(self):
+        for decimal, roman in self.test_cases:
+            self.assertEqual(to_roman(decimal), roman, f'{decimal} convert to {roman}')
 
     def test_type_error_int(self):
         with self.assertRaises(TypeError):
--- tests/test_roman_to_decimal.py
+++ tests/test_roman_to_decimal.py
@@ -5,66 +5,39 @@ from roman import to_decimal
 
 class TestRomanToDecimal(unittest.TestCase):
 
-    def test_convert_to_1(self):
-        self.assertEqual(to_decimal('I'), 1, 'Convert To 1')
+    def setUp(self) -> None:
+        self.test_cases = (
+            (1, 'I'),
+            (3, 'III'),
+            (4, 'IV'),
+            (5, 'V'),
+            (8, 'VIII'),
+            (9, 'IX'),
+            (10, 'X'),
+            (38, 'XXXVIII'),
+            (40, 'XL'),
+            (50, 'L'),
+            (77, 'LXXVII'),
+            (100, 'C'),
+            (138, 'CXXXVIII'),
+            (170, 'CLXX'),
+            (215, 'CCXV'),
+            (238, 'CCXXXVIII'),
+            (390, 'CCCXC'),
+            (483, 'CDLXXXIII'),
+            (500, 'D'),
+            (1000, 'M'),
+            (1198, 'MCXCVIII'),
+            (1240, 'MCCXL'),
+        )
 
-    def test_convert_to_5(self):
-        self.assertEqual(to_decimal('V'), 5, 'Convert To 5')
-
-    def test_convert_to_10(self):
-        self.assertEqual(to_decimal('X'), 10, 'Convert To 10')
-
-    def test_convert_to_50(self):
-        self.assertEqual(to_decimal('L'), 50, 'Convert To 50')
-
-    def test_convert_to_100(self):
-        self.assertEqual(to_decimal('C'), 100, 'Convert To 100')
-
-    def test_convert_to_500(self):
-        self.assertEqual(to_decimal('D'), 500, 'Convert To 500')
-
-    def test_convert_to_1000(self):
-        self.assertEqual(to_decimal('M'), 1000, 'Convert To 1000')
-
-    def test_convert_to_3(self):
-        self.assertEqual(to_decimal('III'), 3, 'Convert To 2')
-
-    def test_convert_to_9(self):
-        self.assertEqual(to_decimal('IX'), 9, 'Convert To 9')
-
-    def test_convert_to_14(self):
-        self.assertEqual(to_decimal('XIV'), 14, 'Convert To 14')
-
-    def test_convert_to_38(self):
-        self.assertEqual(to_decimal('XXXVIII'), 38, 'Convert To 38')
-
-    def test_convert_to_77(self):
-        self.assertEqual(to_decimal('LXXVII'), 77, 'Convert To 77')
-
-    def test_convert_to_92(self):
-        self.assertEqual(to_decimal('XCII'), 92, 'Convert To 92')
-
-    def test_convert_to_238(self):
-        self.assertEqual(to_decimal('CCXXXVIII'), 238, 'Convert To 238')
-
-    def test_convert_to_714(self):
-        self.assertEqual(to_decimal('DCCXIV'), 714, 'Convert To 714')
-
-    def test_convert_to_1198(self):
-        self.assertEqual(to_decimal('MCXCVIII'), 1198, 'Convert To 1198')
-
-    def test_convert_to_1240(self):
-        self.assertEqual(to_decimal('MCCXL'), 1240, 'Convert To 1240')
-
-    def test_convert_to_504(self):
-        self.assertEqual(to_decimal('DIV'), 504, 'Convert To 504')
+    def test_roman_to_decimal(self):
+        for decimal, roman in self.test_cases:
+            self.assertEqual(to_decimal(roman), decimal, f'{roman} convert to {decimal}')
 
     def test_type_error_int(self):
         with self.assertRaises(TypeError):
             to_decimal(1)
-
-    def test_type_error_list(self):
-        with self.assertRaises(TypeError):
             to_decimal(['I', 'XIV'])
 
 
